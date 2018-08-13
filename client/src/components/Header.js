4a7f3cb6eb2fdb620c9af24fd0ea58faeae46537
Pob <590650@gmail.com>
2018-08-13 14:37:32 +0700

    working on Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+
+class Header extends Component {
+  renderMenu = () => {
+    switch(this.props.auth) {
+      case null:
+        // deciding whether an user is logged in or not
+        return
+      case false:
+        // an user is not logged in
+        return <li><a href="/auth/google">Log in With Google</a></li>
+      default:
+        // an user is already logged in
+        return <li><a href="/api/logout">Log Out</a></li>
+    }
+  }
+  
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="brand-logo">Emaily</a>
+          <ul className="right">
+            { this.renderMenu() }
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+const mapStateToProps = ({ auth }) => { 
+  return { auth } 
+}
+
+export default connect(mapStateToProps)(Header)
---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h2>Emaily</h2>
+      Collect your users' feedback via Emaily :)
+    </div>
+  )
+}
+
+export default Landing
