039ee1dc149083a2ad8e5d41b63e7a500062cb86
Pob <590650@gmail.com>
2018-08-16 12:25:16 +0700

    add a target link of a logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import Payments from './Payments'
 
 class Header extends Component {
-  renderMenu = () => {
+  renderContent = () => {
     switch (this.props.auth) {
       case null:
         // deciding whether an user is logged in or not
@@ -36,8 +37,10 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="brand-logo">Emaily</a>
-          <ul className="right">{this.renderMenu()}</ul>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     )
