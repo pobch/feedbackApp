5f9068298f2a1833898c1b8ec5d7c482cdbe76b3
Pob <590650@gmail.com>
2018-09-02 06:35:59 +0700

    add submitSurvey action to call backend's api

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -18,6 +18,11 @@ export const handleToken = token => async dispatch => {
   })
 }
 
-export const submitSurvey = values => async dispatch => {
-  console.log(values)
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values)
+  dispatch({
+    type: FETCH_USER,
+    payload: res.data
+  })
+  history.push('/surveys')
 }
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 import * as actions from '../../actions'
 import formFields from './formFields'
 
-const SurveyFormReview = ({ onBack, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onBack, formValues, submitSurvey, history }) => {
   const renderReview = _.map(formFields, ({ label, name }) => {
     return (
       <React.Fragment key={name}>
@@ -22,7 +23,7 @@ const SurveyFormReview = ({ onBack, formValues, submitSurvey }) => {
       <button type="button" onClick={onBack}>
         Back
       </button>
-      <button type="button" onClick={() => submitSurvey(formValues)}>
+      <button type="button" onClick={() => submitSurvey(formValues, history)}>
         Submit
       </button>
     </div>
@@ -38,4 +39,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   actions
-)(SurveyFormReview)
+)(withRouter(SurveyFormReview))
