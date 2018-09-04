e8a3dd30ea0be88e5f8651b21f9060bd1b4a52e8
Pob <590650@gmail.com>
2018-09-05 01:54:30 +0700

    testing webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+const Path = require('path-parser').default
+const { URL } = require('url')
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredit = require('../middlewares/requireCredit')
@@ -9,6 +12,16 @@ const Survey = mongoose.model('Survey')
 module.exports = app => {
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your feedback!')
+  })
+
+  app.post('/api/surveys/webhook', (req, res) => {
+    const events = _.map(req.body, ({ url }) => {
+      const pathname = new URL(url).pathname
+      const p = new Path('/api/surveys/:surveyId/:choice')
+      return p.test(pathname)
+    })
+    console.log(events)
+    res.send('OK')
   })
 
   app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
